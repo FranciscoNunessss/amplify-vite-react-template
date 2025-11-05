71fd54181d4e7e2fcfe5b58af400f2aa80ec32ec
Francisco <a2022147843@alumni.iscac.pt>
2025-11-05 17:04:50 +0000

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,56 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { signOut, user } = useAuthenticator((context) => [context.user]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const sub = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+    return () => sub.unsubscribe();
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    const content = window.prompt("Todo content");
+    if (content) {
+      client.models.Todo.create({ content });
+    }
+  }
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
+    <main className="p-4">
+      <header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: 16 }}>
+        <h1>My todos</h1>
+        <div style={{ display: "flex", alignItems: "center", gap: 12 }}>
+          {user?.username && <span>Signed in as <strong>{user.username}</strong></span>}
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      </header>
+
       <button onClick={createTodo}>+ new</button>
+
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id}>
+            {todo.content}
+            <button onClick={() => deleteTodo(todo.id)} style={{ marginLeft: 8 }}>
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
-      <div>
+
+      <div style={{ marginTop: 24 }}>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
