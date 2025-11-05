4ce1b2aeecca9ca219ed66c5ce5f9f6374abb991
Francisco <a2022147843@alumni.iscac.pt>
2025-11-05 17:42:14 +0000

    upload css

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,47 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from "@aws-amplify/ui-react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
+  // agora também trazemos o user
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { user, signOut } = useAuthenticator((context) => [context.user]);
-
+  
   useEffect(() => {
-    const sub = client.models.Todo.observeQuery().subscribe({
+    client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-    return () => sub.unsubscribe();
   }, []);
 
   function createTodo() {
-    const content = window.prompt("Todo content");
-    if (content) {
-      client.models.Todo.create({ content });
-    }
+    client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
   function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
+    client.models.Todo.delete({ id })
   }
-
+  
   return (
-    <main className="p-4">
-      <header
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: 16,
-        }}
-      >
-        {/* ✅ mostra o loginId (ex: email ou username) do usuário autenticado */}
-        <h1>{user?.signInDetails?.loginId}'s todos</h1>
-        <div style={{ display: "flex", alignItems: "center", gap: 12 }}>
-          {user?.username && (
-            <span>
-              Signed in as <strong>{user.username}</strong>
-            </span>
-          )}
-          <button onClick={signOut}>Sign out</button>
-        </div>
-      </header>
-
+    <main>
+      {/* renderiza o loginId do utilizador autenticado */}
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
-
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.content}
-            <button
-              onClick={() => deleteTodo(todo.id)}
-              style={{ marginLeft: 8 }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
+        {todos.map(todo => <li
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+          {todo.content}
+        </li>)}
       </ul>
-
-      <div style={{ marginTop: 24 }}>
+      <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next step of this tutorial.</a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
